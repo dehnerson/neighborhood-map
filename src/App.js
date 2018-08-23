310e4514fb43ab79f6c0145bd41e348439018b2f
Sascha Dehler <saschadehler@gmail.com>
2018-08-23 14:34:33 +0200

    feat: Included Map component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Map from './Map';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Map/>
       </div>
     );
   }
